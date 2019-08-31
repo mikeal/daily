ae2f1334975f77e751c1f409259783352dfbda69
Mikeal Rogers <mikeal.rogers@gmail.com>
2019-08-31 01:51:21 +0000

    feat: pull and a few days to work with

--- cli.js
+++ cli.js
@@ -20,16 +20,36 @@ const pullHour = async argv => {
   }
 }
 
+const pullRange = async argv => {
+  let start = new Date(argv.starttime)
+  const end = new Date(argv.endtime)
+  let output = argv.output
+  if (output && !output.endsWith('/')) output += '/'
+  while (start < end) {
+    argv.datetime = start
+    if (output) {
+      console.log('pulling ' + start)
+      let filename = min.tsToFilename(start)
+      filename = filename.slice(0, filename.length - 3)
+      console.log({filename}, filename.length - 3)
+      argv.output = output + filename
+    }
+    await pullHour(argv)
+    start = new Date(start.getTime() + onehour)
+  }
+}
+
 const outputOptions = yargs => {
   yargs.option('output', {
     alias: 'o',
-    description: 'File to output to.'
+    description: 'Output file or directory.'
   })
 }
 
 const yargs = require('yargs')
 const args = yargs
   .command('pull-hour [datetime]', 'pull an hour of gharchive', outputOptions, pullHour) 
+  .command('pull <starttime> <endtime>', 'pull a timerange', outputOptions, pullRange)
   .argv
 
 if (!args._.length) {
